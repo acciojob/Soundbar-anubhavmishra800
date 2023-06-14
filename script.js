a92ef508d3b900b5335ee2b8d571506386683fcb
anubhavmishra800 <127845673+anubhavmishra800@users.noreply.github.com>
2023-06-14 12:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+let audios = [
+                "applause",
+                "boo",
+                "gasp",
+                "tada",
+                "victory",
+                "wrong"
+];
+let activeAudio = null ;
+const buttonss = document.getElementsByTagName("button");
+
+function playAudio(){
+    stopAudio();
+    activeAudio.play();
+}
+function stopAudio(){
+    if(activeAudio){
+		activeAudio.pause();
+	}
+}
+
+for(let i = 0; i < buttonss.length; i++) {
+    buttonss[i].addEventListener("click", (event) => {
+        let innerText = event.target.innerText ;
+        if(audios.includes(innerText)){
+            // play that particular audio
+            let audioElement = document.createElement("audio");
+            audioElement.src = `./audio/${innerText}.mp3`;
+            audioElement.controls = true ;
+            activeAudio = audioElement;
+			playAudio();
+		}
+		else {
+			stopAudio();
+		}
+	})
+}
